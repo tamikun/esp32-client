c2a954cf40fa653d1a3af51b84a370454ac78195
Tang Minh Quan <quantm@jits.vn>
2023-07-04 09:38:11 +0700

    move instance listserver

---
+++ src/esp32-client/Services/Services/ListServer.cs
@@ -0,0 +1,54 @@
+using esp32_client.Models;
+
+namespace esp32_client.Services
+{
+    public class ListServer
+    {
+        private static ListServer? instance;
+        private List<ServerModel> itemList;
+        private readonly IClientService _clientService;
+
+        // Private constructor to prevent direct instantiation
+        private ListServer(IClientService clientService)
+        {
+            _clientService = clientService;
+
+            // Initialize the list of items
+            itemList = CreateItemList();
+        }
+
+        // Public method to get the instance of the singleton class
+        public static ListServer GetInstance(IClientService clientService)
+        {
+            // Create a new instance if it doesn't exist
+            if (instance == null)
+            {
+                instance = new ListServer(clientService);
+            }
+
+            return instance;
+        }
+
+        // Method to retrieve the list of items
+        public List<ServerModel> GetItemList()
+        {
+            return itemList;
+        }
+
+        // Your method to create the list of items
+        private List<ServerModel> CreateItemList()
+        {
+            // Implementation to create and return the list of items
+            var response = _clientService.GetAvailableIpAddress();
+
+            return response;
+        }
+
+        // Reload
+        public void ReloadItemList()
+        {
+            // Implementation to create and return the list of items
+            itemList = _clientService.GetAvailableIpAddress();
+        }
+    }
+}
