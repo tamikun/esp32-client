002d007f9686a7a39d1b34b867a7bbda6e8768bf
Tang Minh Quan <quantm@jits.vn>
2023-07-10 08:20:12 +0700

    update

--- src/publish.bat
+++ src/publish.bat
@@ -2,6 +2,12 @@
 cd /d D:\MiQ\MiQ_NT\ESP32Client\esp32-client\src
 dotnet publish esp32-client\esp32-client.csproj -c Release -o ShareFiles\esp32-client\publish
 
+REM Check if the publish command was successful
+IF %errorlevel% NEQ 0 (
+  echo Failed to publish the project. Exiting...
+  exit /b 1
+)
+
 cd ShareFiles
 docker compose up -d --build esp32client.api
-pause
+REM pause
