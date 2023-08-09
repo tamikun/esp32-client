b153e7270418eddde264628fd8395fa8980afce2
Tang Minh Quan <quantm@jits.vn>
2023-08-09 14:20:03 +0700

    remove unuse

--- src/esp32-client/Models/DataFileModel.cs
+++ src/esp32-client/Models/DataFileModel.cs
@@ -1,30 +1,9 @@
 namespace esp32_client.Models;
 
-public class DataFileModel
-{
-    public string? FilePath { get; set; }
-
-    public string? FileType { get; set; }
-
-    public long? FileSize { get; set; }
-
-}
 
 public class SelectedDataFileModel
 {
     public string? FilePath { get; set; }
 
     public bool IsSelected { get; set; } = false;
-}
-
-public class FileSystemRequestModel
-{
-    public FileSystemRequestModel()
-    {
-        ListDeleteFile = new List<SelectedDataFileModel>();
-    }
-
-    public IFormFileCollection? ListUploadFile { get; set; }
-    public string? Folder { get; set; }
-    public List<SelectedDataFileModel> ListDeleteFile { get; set; }
 }
