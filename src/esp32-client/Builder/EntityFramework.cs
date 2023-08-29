e4e2673f3ebd894b114571151666a7664fe68019
Tang Minh Quan <quantm@jits.vn>
2023-08-29 15:41:11 +0700

    .

--- src/esp32-client/Builder/EntityFramework.cs
+++ src/esp32-client/Builder/EntityFramework.cs
@@ -12,22 +12,5 @@ namespace esp32_client.Builder
         {
             Factory = Set<Factory>();
         }
-
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
-        //     optionsBuilder.UseSqlite("Data Source=hello.db;");
-        // }
-
-        // protected override void OnModelCreating(ModelBuilder modelBuilder)
-        // {
-        //     base.OnModelCreating(modelBuilder);
-
-        // }
-
-
-        // public async Task<List<Factory>> GetListFactory()
-        // {
-        //     return await Factory.ToListAsync();
-        // }
     }
 }
