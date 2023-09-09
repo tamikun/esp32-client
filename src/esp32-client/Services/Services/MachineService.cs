b012207d390531a39340ba4a1e982b393de8bcd0
Tang Minh Quan <quantm@jits.vn>
2023-09-09 10:36:09 +0700

    Add validation according current board

--- src/esp32-client/Services/Services/MachineService.cs
+++ src/esp32-client/Services/Services/MachineService.cs
@@ -585,8 +585,8 @@ public partial class MachineService : IMachineService
         if (!System.IO.File.Exists(_settings.MachineFirmwareFilePath))
             throw new Exception("Firmware is not found");
 
-        // if (ipAddress != _settings.DefaultNewMachineIp)
-        //     throw new Exception("Please reset machine system before updating firmware!");
+        if (ipAddress != _settings.DefaultNewMachineIp)
+            throw new Exception("Please reset machine system before updating firmware!");
 
         // var machine = await GetByIpAddress(ipAddress);
 
@@ -617,8 +617,8 @@ public partial class MachineService : IMachineService
     /// <returns></returns>
     public async Task<(bool Success, string ResponseBody)> UpdateAddress(string currentIpAddress, string newIpAddress)
     {
-        // if (currentIpAddress != _settings.DefaultNewMachineIp)
-        //     throw new Exception("Please reset machine system before updating ip address!");
+        if (currentIpAddress != _settings.DefaultNewMachineIp)
+            throw new Exception("Please reset machine system before updating ip address!");
 
         if (await _linq2db.Entity<Machine>().AnyAsync(s => s.IpAddress == newIpAddress))
             throw new Exception("New ip address has aldready exited!");
